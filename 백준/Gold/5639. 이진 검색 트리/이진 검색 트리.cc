9404c682da5c0dcbf2cbc883f7df54ae407b5a75
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2025-09-14 19:05:55 +0900

    [Gold IV] Title: 이진 검색 트리, Time: 52 ms, Memory: 2500 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> pre; // 전위순회 입력,모두 다른 값
+int n;
+
+void post(int start, int end) {
+	if (start >= end) return;
+
+	int root = pre[start];
+	int i = start + 1;
+	for (; i < end; i++) {
+		if (root < pre[i]) break;
+	}
+	post(start + 1, i); // left
+	post(i, end); // right
+
+	cout << root << '\n';
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int x;
+	while (cin >> x) {
+		pre.push_back(x);
+	}
+
+	post(0, pre.size());
+	
+	return 0;
+}
