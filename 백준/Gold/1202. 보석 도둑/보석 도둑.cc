d29da7cb93d02fd2fe8ed45079b87888123ee79e
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2025-09-15 16:51:05 +0900

    [Gold II] Title: 보석 도둑, Time: 156 ms, Memory: 8740 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int n, k;
+vector<pair<int, int>> jew; // 보석 {무게, 가격}
+vector<int> c; // 가방 최대 수용 무게
+
+priority_queue<int> pq; // 가격, max heap
+int jew_idx = 0;
+
+int bag_max_price(int c_idx) {
+	while (jew_idx < n && c[c_idx] >= jew[jew_idx].first) {
+		pq.push(jew[jew_idx++].second);
+	}
+
+	if (!pq.empty()) {
+		int p = pq.top();
+		pq.pop();
+
+		return p;
+	}
+
+	return 0;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n >> k;
+	jew.resize(n);
+	c.resize(k);
+
+	for (int i = 0; i < n; i++) {
+		cin >> jew[i].first >> jew[i].second;
+	}
+
+	for (int i = 0; i < k; i++) {
+		cin >> c[i];
+	}
+
+	// 무게 오름차순 정렬
+	sort(jew.begin(), jew.end());
+	sort(c.begin(), c.end());
+
+	long long sum = 0;
+	for (int i = 0; i < k; i++) {
+		// 수용 무게 적은 가방부터 최대 가격으로 채우기
+		sum += bag_max_price(i);
+	}
+	
+	cout << sum << '\n';
+
+	return 0;
+}
