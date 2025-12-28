c663552b7366d897ab3095b609d6905dfbba4fa7
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2025-12-28 18:05:32 +0900

    [level 2] Title: 모음 사전, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -4,31 +4,28 @@
 
 using namespace std;
 
-char vowel[5] = {'A', 'E', 'I', 'O', 'U'};
-int cnt = 0;
-bool check = false;
-
-void dfs(int depth, string cur, string target){
-    if(check) return;
-    
-    if(depth) {
-        cnt++;
-        if(cur == target) {
-            check = true;
-            return;
-        }
-    }
-    if(depth == 5) return;
-    
-    for(int i=0; i<5; i++){
-        cur.push_back(vowel[i]);
-        dfs(depth + 1, cur, target);
-        cur.pop_back();
-    }
-}
-
 int solution(string word) {
-    dfs(0, "", word);
-    int answer = cnt;
+    /*
+    a----: 1 ~ 781번째
+    e----: 781*1 + 1 ~ 
+    i----: 781*2 + 1 ~
+    
+    aa---: 1 ~ 156
+    ae---: 156*1 + 1 ~
+    ea---: (781*1 + 1) + (1) ~
+    ee---: (781*1 + 1) + (156*1 + 1) ~
+    */
+    int weight[5] = {781, 156, 31, 6, 1};
+    int answer = 0;
+    for(int i=0; i<word.length(); i++){
+        int x;
+        if(word[i] == 'A') x = 0;
+        else if(word[i] == 'E') x = 1;
+        else if(word[i] == 'I') x = 2;
+        else if(word[i] == 'O') x = 3;
+        else if(word[i] == 'U') x = 4;
+        
+        answer += weight[i] * x + 1;
+    }
     return answer;
 }
