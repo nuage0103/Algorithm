71fa29d648e51bd1eac2f81ec1d9f216b458a60d
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-01-05 17:13:21 +0900

    [Gold V] Title: 달력, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.cc
+++ 백준/Gold/20207. 달력/달력.cc
@@ -4,10 +4,6 @@
 using namespace std;
 
 typedef pair<int, int> pii;
-/*
-index tree(segment tree): update와 query 계속 발생, 중간에 발생하는 query에 대한 응답처리 가능
-difference array: 마지막에 몰아서 update, query(최종값. 누적합)
-*/
 
 int main(){
     ios::sync_with_stdio(0);
@@ -17,13 +13,12 @@ int main(){
     int n;
     cin >> n;
     vector<pii> cal;
-    int diff[367] = {0};
+    int freq[367] = {0};
+    int s, e;
     for(int i=0; i<n; i++){
-        int s, e;
         cin >> s >> e;
         cal.push_back({s, e});
-        diff[s]++;
-        diff[e + 1]--;
+        for(int i = s; i <= e; i++) freq[i]++;
     }
 
     sort(cal.begin(), cal.end(), [](pii& a, pii& b){
@@ -35,28 +30,25 @@ int main(){
 
     int start = cal[0].first;
     int end = cal[n-1].second;
-    for(int i = 1; i < 366; i++){
-        diff[i + 1] += diff[i];
-    }
-
     int res = 0;
-    int width = 0;
-    int height = -1e9;
     while(start <= end){
-        if(!diff[start]) {
+        if(!freq[start]){
             start++;
             continue;
         }
 
-        while(diff[start] > 0) {
+        int width = 0;
+        int height = -1e9;
+        while(freq[start]){
             width++;
-            height = max(height, diff[start]);
+            height = max(height, freq[start]);
             start++;
         }
         res += width * height;
         width = 0;
         height = -1e9;
     }
+
     cout << res << '\n';
 
     return 0;
