32b7afdc5a2f51e8de12cd819ea3a977dd0a634c
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-02-11 16:39:16 +0900

    [Silver V] Title: 배열 합치기, Time: 428 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<m; i++){
+        cin >> b[i];
+    }
+
+    int a_idx=0, b_idx=0;
+    while(a_idx < n && b_idx < m){
+        if(a[a_idx] < b[b_idx]) cout << a[a_idx++] << ' ';
+        else cout << b[b_idx++] << ' ';
+    }
+    while(a_idx < n) cout << a[a_idx++] << ' ';
+    while(b_idx < m) cout << b[b_idx++] << ' ';
+    cout << '\n';
+
+    return 0;
+}
