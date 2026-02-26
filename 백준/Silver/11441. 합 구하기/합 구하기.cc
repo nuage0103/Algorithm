9e0b787410c340804b8164ef563b201e7850a27c
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-02-26 15:54:56 +0900

    [Silver III] Title: 합 구하기, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.cc
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -1,48 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> inp;
-vector<int> idx_tree;
-int leaf;
-
-void make_tree(int n){
-    leaf = 1;
-    while(leaf < n) leaf *= 2;
-    idx_tree.resize(leaf * 2);
-
-    for(int i = 0; i < n; i++){
-        int idx = leaf + i;
-        idx_tree[idx] = inp[i];
-
-        idx /= 2;
-        while(idx){
-            idx_tree[idx] = idx_tree[idx * 2] + idx_tree[idx * 2 + 1];
-            idx /= 2;
-        }
-    }
-}
-
-int query(int st, int en){
-    st = leaf + (st - 1);
-    en = leaf + (en - 1);
-
-    int sum = 0;
-    while(st <= en){
-        if(st % 2 == 1){
-            sum += idx_tree[st];
-            st++;
-        }
-        if(en % 2 == 0){
-            sum += idx_tree[en];
-            en--;
-        }
-
-        st /= 2;
-        en /= 2;
-    }
-    return sum;
-}
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -50,18 +8,22 @@ int main(){
 
     int n, m;
     cin >> n;
-    inp.resize(n);
+    vector<int> dp(n); // dp[i] = inp[0] ~ inp[i] 까지 합
+    int sum = 0;
     for(int i = 0; i < n; i++){
-        cin >> inp[i];
+        int x;
+        cin >> x;
+        sum += x;
+        dp[i] = sum;
     }
-
-    make_tree(n);
-
     cin >> m;
     while(m--){
         int i, j;
         cin >> i >> j;
-        cout << query(i, j) << '\n';
+        i--;
+        j--;
+        if(i) cout << dp[j] - dp[i - 1] << '\n';
+        else cout << dp[j] << '\n';
     }
 
     return 0;
