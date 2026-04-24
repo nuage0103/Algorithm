ecb3cc7f23a6a0f4b7c0e43196d18f6271bf2d61
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-04-24 13:54:37 +0900

    [level 3] Title: 징검다리 건너기, Time: 4.94 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -5,33 +5,27 @@
 using namespace std;
 
 int solution(vector<int> stones, int k) {
-    int answer = 0;
+    int answer = 0; // 최대 인원
     
-    vector<int> sorted = stones;
-    int n = stones.size();
-    sort(sorted.begin(), sorted.end());
-
-    int st = sorted[0], en = sorted[n-1];
+    int st = 0, en = *max_element(stones.begin(), stones.end());
     while(st <= en){
-        int mid = (st + en) / 2;
+        int mid = st + (en - st) / 2;
         int skip = 0;
-        bool flag = true;
-        for(int x: stones){
-            if(x - mid < 0) {
+        for(int s: stones){
+            if(s < mid){
                 skip++;
-                if(skip >= k) {
-                    flag = false;
-                    break;
-                }
+                if(skip == k) break;
             }
             else skip = 0;
         }
         
-        if(flag){
-            answer = max(answer, mid);
+        if(skip < k){
+            answer = mid;
             st = mid + 1;
         }
-        else en = mid - 1;
+        else{
+            en = mid - 1;
+        }
     }
     
     return answer;
