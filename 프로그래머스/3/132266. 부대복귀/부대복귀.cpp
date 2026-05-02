877758ccd881524301b2b6fa4306f2eddfcb1c83
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-05-02 15:58:23 +0900

    [level 3] Title: 부대복귀, Time: 2858.24 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
@@ -1,47 +1,9 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
-#include <queue>
 
 using namespace std;
 
-vector<vector<int>> adj;
-
-int bfs(int s, int d, int n){
-    queue<int> q;
-    vector<int> visited(n + 1, -1);
-    q.push(s);
-    visited[s] = 0;
-    while(!q.empty()){
-        int x = q.front();
-        int t = visited[x];
-        q.pop();
-        
-        if(x == d) return t;
-        
-        for(int nx: adj[x]){
-            if(visited[nx] != -1) continue;
-            
-            visited[nx] = t + 1;
-            q.push(nx);
-        }
-    }
-    
-    return -1;
-}
-
 vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
     vector<int> answer;
-    adj.resize(n + 1);
-    for(int i = 0; i < roads.size(); i++){
-        int u = roads[i][0];
-        int v = roads[i][1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    
-    for(int s: sources) answer.push_back(bfs(s, destination, n));
-    
     return answer;
 }
