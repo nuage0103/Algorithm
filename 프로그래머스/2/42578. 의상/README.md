9342aa36213539cd8e4ab4bcd23a3620c8db674e
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-06-26 14:49:12 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 3.92 MB, 시간: 0.03 ms
+메모리: 4.76 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 28일 15:38:18
+2026년 06월 26일 14:49:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,23 +1,27 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 #include <unordered_map>
 
 using namespace std;
 
-int solution(vector<vector<string>> clothes) { // {이름, 종류}
-    unordered_map<string, int> map; // {종류, 이름 개수}. 이름 중복 없음
-    int n = clothes.size();
-    for(int i=0; i<n; i++){
-        string type = clothes[i][1];
-        if(map.find(type) == map.end()) map[type] = 1;
-        else map[type]++;
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    
+    unordered_map<string, int> um; // {종류, 개수}
+    for(int i = 0; i < clothes.size(); i++){
+        // clothes: {이름, 종류}
+        um[clothes[i][1]]++;
     }
     
-    int answer = 1;
-    for(auto& it: map){
-        // 개수 + 아무것도 선택안함(1)
-        answer *= (it.second + 1);
+    // 개수 = 종류별 개수 + 1(선택안함)
+    // answer = 개수들 곱 - 1
+    answer = 1;
+    for(auto& [k, v]: um){
+        answer *= (v + 1);
     }
     answer -= 1;
+    
     return answer;
 }
