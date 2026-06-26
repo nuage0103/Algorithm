1da002f3f768696d1dbc8aec129da1de7a20af3c
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-06-26 15:48:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -10,25 +10,21 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     
     int n = genres.size();
-    unordered_map<string, int> um; // {장르, 횟수 합}
+    unordered_map<string, int> total; // {장르, 횟수 합}
+    unordered_map<string, vector<pair<int, int>>> same_gen; // {장르, {횟수, 고유번호}}
     for(int i = 0; i < n; i++){
-        um[genres[i]] += plays[i];
+        total[genres[i]] += plays[i];
+        same_gen[genres[i]].push_back({plays[i], i});
     }
-    vector<pair<string, int>> freq(um.begin(), um.end());
+    vector<pair<string, int>> gen_order(total.begin(), total.end());
     // 장르 별 횟수 내림차순 정렬
-    sort(freq.begin(), freq.end(), [](const auto& a, const auto& b){
+    sort(gen_order.begin(), gen_order.end(), [](const auto& a, const auto& b){
         return a.second > b.second;
     });
     
-    for(int f = 0; f < freq.size(); f++){
-        string g = freq[f].first;
+    for(auto& [g, v]: gen_order){
+        auto& tmp = same_gen[g];
         
-        vector<pair<int, int>> tmp;
-        for(int i = 0; i < n; i++){
-            if(genres[i] != g) continue;
-            
-            tmp.push_back({plays[i], i});
-        }
         // 동일 장르: 횟수, 고유번호(원본 인덱스)
         sort(tmp.begin(), tmp.end(), [](const auto& a, const auto& b){
             if(a.first != b.first) return a.first > b.first;
@@ -37,7 +33,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
         
         answer.push_back(tmp[0].second);
         if(tmp.size() > 1) answer.push_back(tmp[1].second);
-        tmp.clear();
     }
+    
     return answer;
 }
