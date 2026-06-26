805f351ff0809aaf3e0a6c450eacb1e49c2932a5
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-06-26 16:08:15 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,29 +1,37 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
-vector<int> visited;
-
-void dfs(int x, int n, vector<vector<int>>& computers){
-    visited[x] = 1;
-    for(int i = 0; i < n; i++){
-        if(!computers[x][i]) continue;
-        if(visited[i]) continue;
-        
-        dfs(i, n, computers);
-    }
-}
+// disjoint set / bfs
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     
-    visited.assign(n, 0);
+    vector<int> net(n, -1); // net[i] = i가 속한 네트워크 + 방문여부 확인
     for(int i = 0; i < n; i++){
-        if(!visited[i]){
-            dfs(i, n, computers);
-            answer++;
+        if(net[i] != -1) continue;
+        
+        queue<int> q;
+        q.push(i);
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            net[x] = answer;
+            
+            for(int nx = 0; nx < computers[x].size(); nx++){
+                if(nx == x) continue;
+                if(!computers[x][nx]) continue;
+                if(net[nx] != -1) continue;
+                
+                q.push(nx);
+            }
         }
+        
+        answer++;
     }
     
     return answer;
