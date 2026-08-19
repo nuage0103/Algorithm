ffd4dab5d93f04becb3f6cb52dd4b3903a7a4fa3
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-08-19 15:05:34 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 11.03 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -6,37 +6,39 @@
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    unordered_map<string, int> um;
-    int g_len = gems.size();
-    int n = 0; // 보석 종류
-    for(int i = 0; i < g_len; i++){
-        um[gems[i]]++;
-        if(um[gems[i]] == 1) n++;
+    vector<int> answer;
+    
+    int n = gems.size();
+    int total = 0; // 전체 종류 수
+    unordered_map<string, int> um; // {string, freq}
+    for(auto& g: gems){
+        um[g]++;
+        if(um[g] == 1) total++;
     }
     
-    pair<int, int> ans = {0, g_len};
     int st = 0, en = 0;
-    unordered_map<string, int> tmp;
-    int cnt = 0; // 구간에 포함된 종류
-    while(st < g_len){
-        while(en < g_len && cnt < n){
-            tmp[gems[en]]++;
-            if(tmp[gems[en]] == 1) cnt++;
-            
+    pair<int, int> res = {-1, n};
+    int cur = 0; // 구간에 포함 종류 수
+    unordered_map<string, int> um2;
+    while(st < n){
+        while(en < n && cur < total){
+            um2[gems[en]]++;
+            if(um2[gems[en]] == 1) cur++;
             en++;
         }
-        if(cnt == n){
-            if(((en - 1) - st) < (ans.second - ans.first)) ans = {st, en - 1};
+        
+        if(cur == total){
+            if(res.second - res.first > en - 1 - st){
+                res = {st, en - 1};
+            }
         }
         
-        tmp[gems[st]]--;
-        if(tmp[gems[st]] == 0) cnt--;
+        um2[gems[st]]--;
+        if(um2[gems[st]] == 0) cur--;
         st++;
     }
     
-    vector<int> answer;
-    answer.push_back(ans.first + 1);
-    answer.push_back(ans.second + 1);
-    
+    answer.push_back(res.first + 1);
+    answer.push_back(res.second + 1);
     return answer;
 }
