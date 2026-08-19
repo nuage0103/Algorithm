8837d60ebc158179918ce6e289dffb971c43861a
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-08-19 17:05:05 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,28 +1,27 @@
 -- 코드를 작성해주세요
 WITH T AS(
-    SELECT 
-    (
-        CASE
-        WHEN AVG(SCORE) >= 96 THEN 'S'
-        WHEN AVG(SCORE) >= 90 THEN 'A'
-        WHEN AVG(SCORE) >= 80 THEN 'B'
-        ELSE 'C'
-        END
-    ) AS GRADE, 
-    (
-        CASE
-        WHEN AVG(SCORE) >= 96 THEN 0.2
-        WHEN AVG(SCORE) >= 90 THEN 0.15
-        WHEN AVG(SCORE) >= 80 THEN 0.1
-        ELSE 0
-        END
-    ) AS BONUS, EMP_NO
+    SELECT EMP_NO, 
+        (
+            CASE
+            WHEN AVG(SCORE) >= 96 THEN 'S'
+            WHEN AVG(SCORE) >= 90 THEN 'A'
+            WHEN AVG(SCORE) >= 80 THEN 'B'
+            ELSE 'C'
+            END
+        ) AS GRADE
     FROM HR_GRADE
     GROUP BY EMP_NO
 )
 
-SELECT E.EMP_NO, E.EMP_NAME, T.GRADE, (E.SAL * T.BONUS) AS BONUS
+SELECT E.EMP_NO, E.EMP_NAME, T.GRADE, ROUND((
+    CASE (T.GRADE)
+    WHEN 'S' THEN 0.2
+    WHEN 'A' THEN 0.15
+    WHEN 'B' THEN 0.1
+    ELSE 0
+    END
+) * E.SAL) AS BONUS
 FROM HR_EMPLOYEES AS E
-JOIN T ON T.EMP_NO = E.EMP_NO
-ORDER BY 1
+JOIN T ON E.EMP_NO = T.EMP_NO
+ORDER BY EMP_NO
 ;
