434b7a30a4334e6eca8238f103fd9a17473fa9aa
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-08-20 17:16:21 +0900

    [level 2] Title: [3차] 방금그곡, Time: 0.25 ms, Memory: 5.08 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.cpp
@@ -1,73 +1,64 @@
 #include <string>
 #include <vector>
+#include <sstream>
 #include <iostream>
 
 using namespace std;
 
-string change(string s){
+/*
+처음부터 끝까지 반복 / 처음부터 중간까지 재생
+재생 시작 ~ 끝 / 악보 길이. 음1개 = 1분
+선택 조건: 재생 시간 긴, 먼저 입력된
+
+C# -> c / C -> C
+hh:mm -> h*60+m
+*/
+
+string change(string& s){
     string ret = "";
     for(int i = 0; i < s.size(); i++){
-        if(s[i] == '#'){
-            char last = ret.back();
-            ret.pop_back();
-            ret += tolower(last);
-            continue;
+        if(i + 1 < s.size() && s[i + 1] == '#'){
+            ret += tolower(s[i]);
+            i++;
         }
         else ret += s[i];
     }
-    
     return ret;
 }
 
 string solution(string m, vector<string> musicinfos) {
     string answer = "";
-    /*
-    m: 기억하는 악보
-    시작, 끝, 제목, 악보(원본)
-    시작~끝, 악보 길이 비교 후 악보 자르기/반복
-    m이랑 일치여부
-    */
     
     m = change(m);
-    int max_time = -1;
     answer = "(None)";
+    int max_time = -1;
     for(int i = 0; i < musicinfos.size(); i++){
-        string info = musicinfos[i];
+        stringstream ss(musicinfos[i]);
+        string st, end, title, music;
+        getline(ss, st, ',');
+        getline(ss, end, ',');
+        getline(ss, title, ',');
+        getline(ss, music, ',');
         
-        // 정보 분리
-        int p1 = info.find(',', 0);
-        int p2 = info.find(',', p1 + 1);
-        int p3 = info.find(',', p2 + 1);
-        string st, en, title, melody;
-        st = info.substr(0, p1); // 0~p1-1
-        en = info.substr(p1 + 1, p2 - p1 - 1); // p1+1~p2-1. p2-1-p1
-        title = info.substr(p2 + 1, p3 - p2 - 1); // p2+1~p3-1
-        melody = info.substr(p3 + 1); // p3+1~
+        int st_h = stoi(st.substr(0, 2));
+        int st_m = stoi(st.substr(3));
+        int end_h = stoi(end.substr(0, 2));
+        int end_m = stoi(end.substr(3));
+        int time = (end_h * 60 + end_m) - (st_h * 60 + st_m);
         
-        // 재생 시간
-        int start = stoi(st.substr(0, 2)) * 60 + stoi(st.substr(3));
-        int end = stoi(en.substr(0, 2)) * 60 + stoi(en.substr(3));
-        int total = end - start;
-        
-        // 악보 샵 변환
-        melody = change(melody);
-        
-        // 재생 시간으로 악보 재생성
-        string new_mel = "";
-        for(int j = 0; j < total; j++){
-            new_mel += melody[j % melody.size()];
+        music = change(music);
+        string played = "";
+        for(int j = 0; j < time; j++){
+            played += music[j % music.size()];
         }
         
-        // 새 악보랑 m 비교
-        if(new_mel.find(m) != string::npos){
-            // 재생시간 더 긴
-            if(max_time < total){
-                max_time = total;
+        // 선택 조건: 재생 시간 긴, 먼저 입력된
+        if(played.find(m) != string::npos){
+            if(time > max_time) {
+                max_time = time;
                 answer = title;
             }
-            // 먼저 입력된 (i)
         }
     }
-    
     return answer;
 }
