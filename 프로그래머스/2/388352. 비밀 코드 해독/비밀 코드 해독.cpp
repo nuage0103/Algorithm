cc897761f8cb1922ddaa7d4a4332effee9b970dc
nuage0103 <130099805+nuage0103@users.noreply.github.com>
2026-08-29 13:53:14 +0900

    [level 2] Title: 비밀 코드 해독, Time: 10.70 ms, Memory: 13.7 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.cpp
@@ -16,37 +16,38 @@ void dfs(int depth, int n, vector<int>& cur, int idx, vector<vector<int>>& res){
     }
 }
 
+bool check(vector<int>& r, const vector<vector<int>>& q, const vector<int>& ans){
+    int m = q.size();
+    
+    for(int i = 0; i < m; i++){
+        // r, q[i] 모두 오름차순
+        int ri = 0, qi = 0;
+        int same = 0;
+        while(ri != 5 && qi != 5){
+            if(r[ri] == q[i][qi]){
+                same++;
+                ri++;
+                qi++;
+            }
+            else if(r[ri] > q[i][qi]) qi++;
+            else ri++;
+        }
+
+        if(same != ans[i]) return false;
+    }
+
+    return true;
+}
+
 int solution(int n, vector<vector<int>> q, vector<int> ans) {
     int answer = 0;
-    int m = q.size();
     
     vector<int> cur(5);
     vector<vector<int>> res; // 1~n 5개 숫자 조합
     dfs(0, n, cur, 1, res);
     
-    for(auto& r: res){        
-        bool check = true;
-        for(int i = 0; i < m; i++){
-            // r, q[i] 모두 오름차순
-            int ri = 0, qi = 0;
-            int same = 0;
-            while(ri != 5 && qi != 5){
-                if(r[ri] == q[i][qi]){
-                    same++;
-                    ri++;
-                    qi++;
-                }
-                else if(r[ri] > q[i][qi]) qi++;
-                else ri++;
-            }
-            
-            if(same != ans[i]){
-                check = false;
-                break;
-            }
-        }
-        
-        if(check) answer++;
+    for(auto& r: res){
+        if(check(r, q, ans)) answer++;
     }
     
     return answer;
